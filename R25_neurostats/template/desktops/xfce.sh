e6ce5dbef46d5d1fc9abab2c7f4da7e3ff752e02
mcuma <m.cuma@utah.edu>
2023-02-27 16:48:18 -0700

    update to R25 class

--- R25_neurostats/template/desktops/xfce.sh
+++ R25_neurostats/template/desktops/xfce.sh
@@ -14,6 +14,22 @@ fi
 xfconf-query -c xfce4-session -p /startup/ssh-agent/enabled -n -t bool -s false
 xfconf-query -c xfce4-session -p /startup/gpg-agent/enabled -n -t bool -s false
 
+# Set the desktop background
+# set background image
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /uufs/chpc.utah.edu/sys/ondemand/chpc-class/R25_neurostats/template/desktops/MainLogo_blk_fullcolor.tif
+# set tiled image style
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/image-style -s 2
+
+# Set Firefox default homepage
+# rough info at https://www.commandlinefu.com/commands/view/5314/change-the-homepage-of-firefox
+# at start there's no "browser.startup.homepage" defined, so, add it to prefs.js
+if ! grep -q Website  "$HOME/.mozilla/firefox/*.default-default/prefs.js"; then
+  echo 'user_pref("browser.startup.homepage", "file:///uufs/chpc.utah.edu/sys/ondemand/chpc-class/R25_neurostats/Website/html/index.html");' >> $HOME/.mozilla/firefox/*.default-default/prefs.js
+fi
+# or if exists
+# sed -i 's|\("browser.startup.homepage",\) "\(.*\)"|\1 "file:///uufs/chpc.utah.edu/sys/ondemand/chpc-class/R25_neurostats/Website/html/index.html"|' $HOME/.mozilla/firefox/*.default-default/prefs.js
+
+
 # Disable useless services on autostart
 AUTOSTART="${HOME}/.config/autostart"
 rm -fr "${AUTOSTART}"    # clean up previous autostarts
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Repository of CHPC's Open OnDemand apps for classes
 - most if not all apps have hard-coded job parameters and software versions
 - two apps, CHEN_Jupyter and ATMOS_Synoptic, are used for multiple classes
 - some class apps are not in use at the moment but are kept available
-
+- active apps are set by symbolic links from the class apps location, `/uufs/chpc.utah.edu/sys/ondemand/chpc-class`, to the expected location on `ondemand-class`, `/var/www/ood/apps/sys`.
+- for more details see [Ondemand-info/class.md](https://github.com/CHPC-UofU/OnDemand-info/blob/master/class.md)
